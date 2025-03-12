ffc816f4262a6e69d8368287f763b8c97ef3a016
rlhughes1987 <rlhughes1987@gmail.com>
2025-03-12 15:17:25 +0000

    new folder

---
+++ netbattle/main.lua
@@ -0,0 +1,102 @@
+function love.load()
+
+    sti = require 'libraries/sti'
+
+    love.graphics.setDefaultFilter("nearest", "nearest") --removes blur from scaling
+
+    require './console'
+    require './module'
+    require './nic'
+
+    cons1 = console:create(4)
+    mod_def_1 = module:create("Defender")
+    mod_def_2 = module:create("Defender")
+    mod_def_3 = module:create("Defender")
+    mod_def_4 = module:create("Defender")
+    mod_def_5 = module:create("Defender")
+    mod_def_6 = module:create("Defender")
+    mod_def_7 = module:create("Defender")
+    mod_def_8 = module:create("Defender")
+
+    cons2 = console:create(4)
+    mod_atk_1 = module:create("Attacker")
+    mod_atk_2 = module:create("Attacker")
+    mod_atk_3 = module:create("Attacker")
+    mod_atk_4 = module:create("Attacker")
+    
+    counter = 0
+    
+    cons1:insert(mod_def_1)
+    cons1:insert(mod_atk_1)
+    cons1:insert(mod_def_2)
+    cons1:insert(mod_def_3)
+
+    cons2:insert(mod_def_4)
+    cons2:insert(mod_def_5)
+    cons2:insert(mod_atk_2)
+    cons2:insert(mod_def_6)
+
+    cons1.nic = nic:create(cons1)
+    cons2.nic = nic:create(cons2)
+
+    start_session(cons1, cons2)
+    CONS1HP = cons1.barrier
+    CONS2HP = cons2.barrier
+
+    -- scene control
+    require './scene_organiser'
+    so = scene_organiser:create()
+    current_scene = so:getScene()
+    gameMap = current_scene.map
+    candidate_scene = nil -- if not nil then we should start drawing next scene and updating world objects (in love.update)
+
+    -- assets
+    tiger_module = love.graphics.newImage("assets/tiger-module.png")
+    dnx500 = love.graphics.newImage("assets/rwt-dnx500.png")
+end
+
+function start_session(console_aggressor, console_passive)
+    console_aggressor:connect(console_passive)
+    console_passive:connect(console_aggressor)
+    console_aggressor:activate()
+    console_passive:activate()
+end
+
+function love.update(dt)
+
+    counter = counter + dt
+    if counter > 0.3 then
+        counter = 0
+        cons1:passive()
+        cons2:passive()
+        CONS1HP = cons1.barrier
+        CONS2HP = cons2.barrier
+    end
+end
+
+
+
+function love.draw()
+
+    local cons1_state_message = "C: " .. cons1.state_message
+    local cons2_state_message = "C: " .. cons2.state_message
+    for m=1, #cons1.modules do
+        cons1_state_message = cons1_state_message .. " M:" .. cons1.modules[m].state_message
+    end
+    for m=1, #cons2.modules do
+        cons2_state_message = cons2_state_message .. " M:" .. cons2.modules[m].state_message
+    end
+
+    love.graphics.print("CONS1 state: " .. cons1_state_message, 10, 10)
+    love.graphics.print("CONS2 state: " .. cons2_state_message, 10, 30)
+    love.graphics.print("CONS1 barrier: " .. CONS1HP, 10, 50)
+    love.graphics.print("CONS2 barrier: " .. CONS2HP, 10, 70)
+
+    if(current_scene:getName() == "protobattle") then
+        gameMap:drawLayer(gameMap.layers["Background"])
+        --gameMap:drawLayer(gameMap.layers["Consoles"])
+        love.graphics.draw(dnx500,192,416)
+        love.graphics.draw(tiger_module, 192+cons1.slots.xoff, 416+cons1.slots.yoff)
+    end
+
+end
---
+++ netbattle/scene_organiser.lua
@@ -0,0 +1,40 @@
+require './scene'
+
+scene_organiser = {}
+scene_organiser.__index = scene_organiser
+
+function scene_organiser:create()
+    local sg = {}
+    setmetatable(sg, scene_organiser)
+    
+    sg.scenes = {
+        scene:create("protobattle", 0, 0, "2049"),
+        --scene:create("chasm", 416, 0),
+        --scene:create("pit", 448, 0),
+        --scene:create("garbage", 448,0)
+    }
+    sg.current_scene_index = 1
+    return sg
+end
+
+function scene_organiser:getScene()
+    local c_s = self.scenes[self.current_scene_index]
+    return c_s
+end
+
+function scene_organiser:getNextScene(direction)
+
+    if direction == "base" then
+        --pick a specific scene
+    end
+    --extend for other directions
+
+    -- for now just cycle whatever
+    if self.current_scene_index == #self.scenes then -- reset if at end
+        --to do: do something
+    else
+        self.current_scene_index = self.current_scene_index + 1 --otherwise increment
+    end
+
+    return self:getScene()
+end
